ecc698cdb6711afbb6bc70b67b673096bba96e12
wvdv <wim@vander-vegt.nl>
2018-05-03 16:05:47 +0200

    1) Added missing IWebServiceRequest.

--- src/java/eu/rageproject/asset/manager/ILog.java
+++ src/java/eu/rageproject/asset/manager/ILog.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Open University of the Netherlands
+ * Copyright 2018 Open University of the Netherlands
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
